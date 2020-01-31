6b6b1abd2329d1b629aad09fa9ddcbfccfd45ebe
Kirill Strelbitskiy <33574611+Kirillstrelbitskiy@users.noreply.github.com>
2020-01-31 16:02:01 +0200

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+#define files(name) freopen(name".in", "r", stdin),freopen(name".out", "w", stdout)
+#define fir first
+#define sec second
+#define pb push_back
+#define mk make_pair
+#define all(a) a.begin(),a.end()
+#define len(a) (int)(a.size())
+
+typedef long long ll;
+typedef vector <int> vi;
+typedef vector <long long> vll;
+typedef pair <int, int> pii;
+
+using namespace std;
+
+int main() {
+
+	return 0;
+}
